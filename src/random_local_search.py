0b514f5e68206926d2e402dfd2006862a4ff147d
marotte <joseph.marotte@gmail.com>
2020-02-01 12:06:32 +0100

    random_local_search now inherits MasterMindProblemAbstract

--- src/random_local_search.py
+++ src/random_local_search.py
@@ -1,25 +1,38 @@
 import numpy as np
 from src.utils import *
+from src.mastermind_problem import MasterMindProblemAbstract
 
 
 # Random local search on the disposition to guess
 # Returns the number of calls to number_of_equals_elements made
-def random_local_search(size_of_the_array, number_of_colors, array_to_guess):
-    number_of_call_made = 1
-    guess = generate_random_disposition(size_of_the_array, number_of_colors)
-    current_number_of_equals_elements = number_of_equals_elements(guess, array_to_guess)
-    while current_number_of_equals_elements != size_of_the_array:
-        number_of_call_made += 1
-        id_to_modify = np.random.randint(0, size_of_the_array)
-        old_color = guess[id_to_modify]
-        new_color = np.random.randint(0, number_of_colors)
-        while new_color == old_color:
-            new_color = np.random.randint(0, number_of_colors)
-        guess[id_to_modify] = new_color
+class RandomLocalSearch(MasterMindProblemAbstract):
+    def __init__(self, array_size, number_of_colors):
+        super().__init__(array_size, number_of_colors)
 
-        new_current_number_of_equals_elements = number_of_equals_elements(guess, array_to_guess)
-        if new_current_number_of_equals_elements > current_number_of_equals_elements:
-            current_number_of_equals_elements = new_current_number_of_equals_elements
-        else:
-            guess[id_to_modify] = old_color
-    return number_of_call_made
+    def random_local_search(self):
+        self.number_of_call_made += 1
+        guess = generate_random_disposition(self.array_size, self.number_of_colors)
+        current_number_of_equals_elements = number_of_equals_elements(guess, self.array_to_guess)
+        while current_number_of_equals_elements != self.array_size:
+            self.number_of_call_made += 1
+            id_to_modify = np.random.randint(self.array_size)
+            old_color = guess[id_to_modify]
+            new_color = np.random.randint(self.number_of_colors)
+            while new_color == old_color:
+                new_color = np.random.randint(self.number_of_colors)
+            guess[id_to_modify] = new_color
+
+            new_current_number_of_equals_elements = number_of_equals_elements(guess, self.array_to_guess)
+            if new_current_number_of_equals_elements > current_number_of_equals_elements:
+                current_number_of_equals_elements = new_current_number_of_equals_elements
+            else:
+                guess[id_to_modify] = old_color
+
+    def algorithm(self):
+        self.random_local_search()
+
+    def generate_filename_string(self):
+        return "randomLocalSearch_array_size={}_number_of_colors={}".format(self.array_size, self.number_of_colors)
+
+    def generate_configuration_result(self):
+        return "{} {} {}".format(self.array_size, self.number_of_colors, self.number_of_call_made)
